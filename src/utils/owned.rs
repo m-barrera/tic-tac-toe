7759871cc2780c36ffc74623f317c6df4512adba
m-barrera <Mariano.Barrera@live.de>
2021-12-05 22:09:21 +0100

    removed funct and reassigned to the board struct

--- src/utils/owned.rs
+++
@@ -1,8 +0,0 @@
-use crate::{player::Player, board::Board};
-
-pub fn ownedBy(board: Board, player:Player, row: usize, column: usize) -> bool{
-    match board.fields[row][column] {
-        Some(player) => return true,
-        _ => return false,
-    }
-}
