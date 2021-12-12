95f6dfb9ce519155378b2868ea143a06cd2d9c98
m-barrera <Mariano.Barrera@live.de>
2021-12-12 14:01:33 +0100

    added docs

--- src/board.rs
+++ src/board.rs
@@ -1,13 +1,17 @@
 use crate::utils::{printer};
 use crate::player::{Player};
 
-
+/// BoardStruct holds the current Placed figures by every player
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Board {
     pub fields: [[Option<Player>;3];3]
 }
 
+/// Func Implmentation block of BoarStruct
 impl Board {
+    /// Creates a new Gameboard
+    /// Each field can be filled by a Option<Player> which Player is an enum -> Player::X or Player::O
+    /// Returns an instance of Board
     pub fn new() -> Board {
         return Board {
             fields: [
@@ -18,7 +22,8 @@ impl Board {
         }
     }
 
-
+    /// Draws the board onto the console 
+    /// Referencing self to get the current ocupation of the single fields
     pub fn draw_board(&self) {
             println!("*-----*-----*-----*");
             println!("|{}|{}|{}|", 
@@ -38,6 +43,10 @@ impl Board {
             println!("*-----*-----*-----*");
     }
 
+    /// Checks if horizontal winning move
+    /// self reference for fields
+    /// player referense to check for individual player
+    /// returns a Result<Player, String> -> if Ok(Player) -> Win condition was found
     pub fn check_horizontal(&self, player: Player) -> Result<Player, String>{
 
         for row in 0..2{
@@ -48,6 +57,10 @@ impl Board {
         return Err(String::from("no match"));
     }
 
+    /// Checks if vertical winning move
+    /// self reference for fields
+    /// player referense to check for individual player
+    /// returns a Result<Player, String> -> if Ok(Player) -> Win condition was found
     pub fn check_vertical(&self, player: Player) -> Result<Player, String>{
         for column in 0..2 {
             if self.fields[0][column] == Some(player) &&  self.fields[1][column] == Some(player) && self.fields[2][column] == Some(player){
@@ -57,6 +70,10 @@ impl Board {
         return Err(String::from("no match"));
     }
 
+    /// Checks if diagonal winning move
+    /// self reference for fields
+    /// player referense to check for individual player
+    /// returns a Result<Player, String> -> if Ok(Player) -> Win condition was found
     pub fn check_diagonal(&self, player: Player) -> Result<Player, String>{
         if  self.fields[0][0] == Some(player) &&  self.fields[1][1] == Some(player) &&  self.fields[2][2] == Some(player){
             return Ok(player)
@@ -66,6 +83,8 @@ impl Board {
         return Err(String::from("no match"))
     }
 
+    /// Checks if any move is left to be played or each field is occupied
+    /// returns a boolean
     pub fn check_move_left(&self) -> bool {
         for row in self.fields{
             for cell in row {
@@ -79,6 +98,8 @@ impl Board {
         return false
     }
 
+    /// Check if move is valid and cell is not occupied
+    /// Returns an Result<String,String> -> Ok(String) will proceed the mvoe
     fn check_move (&self, row: usize, column:usize) -> Result<String, String> {
         match &self.fields[row][column]{
             Some(Player::O) => return Err(String::from("allready occupied")),
@@ -87,6 +108,8 @@ impl Board {
         }
     }
 
+    /// Fillls up a cell if move is valid
+    /// return a Result<String,String> -> Ok valid move cell will be filled
     pub fn fill_cell(&mut self, row: usize, column: usize, player:Player ) -> Result<String, String> {
         match self.check_move(row, column) {
             Ok(_) => (),
